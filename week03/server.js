9e94b5cf27d80a0df9a528515f2c14f606441a4e
huyingjian <huyingjian@zbj.com>
2020-08-16 22:41:45 +0800

    feat:week03

---
+++ week03/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+    let body = [];
+    request.on('error', (error) => {
+        console.error(error.message)
+    }).on('data', (chunk) => {
+        console.log(chunk.toString())
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('body: ', body)
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.end(fs.readFileSync('./1.html'));
+    });
+}).listen(8088);
